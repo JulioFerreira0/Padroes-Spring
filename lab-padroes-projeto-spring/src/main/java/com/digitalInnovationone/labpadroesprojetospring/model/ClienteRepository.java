b8ab3d5490590576c76c7be4f0e9eae41b2ae7b3
Eskilon <Julio.Jesus@globalweb.com.br>
2021-11-25 17:03:32 -0300

    Projeto DIO

---
+++ lab-padroes-projeto-spring/src/main/java/com/digitalInnovationone/labpadroesprojetospring/model/ClienteRepository.java
@@ -0,0 +1,6 @@
+package com.digitalInnovationone.labpadroesprojetospring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
